854cbd048ba304a2c85dc3df1670f56731399414
Lior Shtaimberg <liorsht@wix.com>
2022-03-14 16:03:08 +0200

    Vagrant setup fix: wasnt migrating initially

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ cd /vagrant
 # Install dependencies with Pipenv
 pipenv sync --dev
 
+# Run database migrations
+pipenv run python manage.py migrate
 # run our app. Nohup and “&” are used to let the setup script finish
 # while our app stays up. The app logs will be collected in nohup.out
 nohup pipenv run python manage.py runserver 0.0.0.0:8000 &
