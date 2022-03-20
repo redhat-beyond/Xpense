6397ee4d08aa3defe143c5362bbf8a8849bd648b
Rony Yosef <ronyyo@mta.ac.il>
2022-03-20 12:38:34 +0200

    add Expenses model with category

---
+++ expenses/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Expenses(models.Model):
+    class Category(models.TextChoices):
+        RENT = 'Rent'
+        MORTGAGE = 'Mortgage'
+        BILLS = 'bills'
+        TRANSPORTATION = 'Transportation'
+        CLOTHING = 'Clothing'
+        HEALTHCARE = 'Healthcare'
+        FOOD = 'Food'
+        INSURANCE = 'Insurance'
+        KIDS = 'Kids'
+        CULTURE = 'Culture'
+        VACATIONS = 'Vacations'
+        OTHER = 'Other'
+
+    category = models.CharField(
+        max_length=32,
+        choices=Category.choices,
+        default=Category.OTHER
+    )
+
+    def __str__(self):
+        return self.category
