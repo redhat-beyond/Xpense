7a0cb6c3e423ee1cf93997edfe45d1f3e8ccba85
Rony Yosef <ronyyo@mta.ac.il>
2022-04-04 13:38:08 +0300

    Init routing added dashboard + how to use with parameters from get request added house_view view that get the house id and return the house information added add_house api to add house with post request
    we will need to add the Tips routing after the app will be marge

---
+++ house/views.py
@@ -0,0 +1,20 @@
+
+
+def home_page(request):
+    raise NotImplementedError
+
+
+def global_page(request):
+    raise NotImplementedError
+
+
+def house_login(request):
+    raise NotImplementedError
+
+
+def house_view(request, house_id):
+    raise NotImplementedError
+
+
+def add_house(request):
+    raise NotImplementedError
