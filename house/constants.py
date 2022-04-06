fe1ecef05119d3dbc4b5acec6a30879f63e82b13
Lior Shtaimberg <liorsht@wix.com>
2022-04-06 22:31:35 +0300

    Create home and base html initial code
    using extends base as the way in django and importing necessary libs like (bootstrap, jquery, chartjs) in base.html for future uses
    also creating the app_layout css for styling the pages

--- house/constants.py
+++ house/constants.py
@@ -1,2 +1,5 @@
 HOME_PAGE_ROUTE = 'home.html'
+<<<<<<< HEAD
 GLOBAL_PAGE_ROUTE = 'global/global.html'
+=======
+>>>>>>> 31ddc69 (Create home and base html initial code)
--- house/views.py
+++ house/views.py
@@ -1,10 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from django.shortcuts import render
 from house.constants import HOME_PAGE_ROUTE, GLOBAL_PAGE_ROUTE
 
 
 =======
 >>>>>>> 2bd9f3f (init routing)
+=======
+from django.shortcuts import render
+from house.constants import HOME_PAGE_ROUTE
+
+
+>>>>>>> 31ddc69 (Create home and base html initial code)
 def home_page(request):
     return render(request, HOME_PAGE_ROUTE)
 
