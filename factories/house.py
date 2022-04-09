c663cf6f05ad5403705d22b6372687eb69b5e161
Rony Yosef <ronyyo@mta.ac.il>
2022-04-10 00:08:57 +0300

    Add scripts to crete mock data for our project installing django-extensions and factory_boy

---
+++ factories/house.py
@@ -0,0 +1,23 @@
+import random
+
+import factory
+from factory.django import DjangoModelFactory
+
+from house.models import House, Country, City, Job
+
+
+class HouseFactory(DjangoModelFactory):
+    class Meta:
+        model = House
+
+    name = factory.Faker('name')
+    public = factory.Faker('boolean')
+    country = factory.Iterator(Country.objects.all())
+    city = factory.LazyAttribute(
+        lambda x: random.choice(City.objects.filter(country=x.country)))
+    parent_profession_1 = factory.LazyAttribute(
+        lambda x: random.choice([tup[1] for tup in Job.choices]))
+    parent_profession_2 = factory.LazyAttribute(
+        lambda x: random.choice([tup[1] for tup in Job.choices]))
+    income = factory.Faker('random_number', digits=5)
+    children = factory.Faker('random_number', digits=1)
