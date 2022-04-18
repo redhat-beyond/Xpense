066aabbd31e2436c6cb62f6031437aa813097cf4
rony yosef <61323123+ronyyosef@users.noreply.github.com>
2022-04-18 16:50:01 +0300

    Add Global page charts
    Add the two charts in the global page

--- expenses/test/test_expenses.py
+++ expenses/test/test_expenses.py
@@ -1,13 +1,15 @@
 import pytest
 from django.utils import timezone
 from expenses.models import Expenses
+from factories.house import HouseFactory
 from house.models import House, Country, City
 
 AMOUNT = '100'
 DATE = timezone.now()
 CATEGORY = 'Clothing'
 
-HOUSE_NAME = "House 1"
+HOUSE_NAME_1 = "House 1"
+HOUSE_NAME_2 = "House 2"
 HOUSE_PUBLIC = True
 COUNTRY = "Colorado"
 CITY = "South Park"
@@ -30,7 +32,7 @@ def generate_city(generate_country):
 @pytest.fixture
 def generate_house(generate_country, generate_city):
     house = House.create_house(
-        name=HOUSE_NAME,
+        name=HOUSE_NAME_1,
         public=HOUSE_PUBLIC,
         country=generate_country,
         city=generate_city,
@@ -67,3 +69,23 @@ class TestExpensesModel:
         generate_expense.save()
         generate_expense.house_name.delete()
         assert generate_expense not in Expenses.objects.all()
+
+
+def _helper_create_houses():
+    house_1 = HouseFactory(name=HOUSE_NAME_1)
+    house_2 = HouseFactory(name=HOUSE_NAME_2)
+    Expenses(house_name=house_1, amount=100, date=DATE, category=Expenses.Category.FOOD).save()
+    Expenses(house_name=house_1, amount=200, date=DATE, category=Expenses.Category.FOOD).save()
+    Expenses(house_name=house_2, amount=300, date=DATE, category=Expenses.Category.FOOD).save()
+    Expenses(house_name=house_2, amount=300, date=DATE, category=Expenses.Category.KIDS).save()
+    Expenses(house_name=house_2, amount=400, date=DATE, category=Expenses.Category.KIDS).save()
+
+
+@pytest.mark.django_db()
+class TestExpensesFunctions:
+    def test_average_expenses_of_houses_by_categories(self):
+        _helper_create_houses()
+        houses = House.objects.all()
+        avg_expenses_by_categories = Expenses.average_expenses_of_houses_by_categories(houses=houses)
+        assert avg_expenses_by_categories[0]['average'] == 200
+        assert avg_expenses_by_categories[1]['average'] == 350
